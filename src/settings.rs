bfb23f30d9bcd0cf5ee57abab6d8565dbeb5269f
LoZander <lozander@proton.me>
2025-12-17 04:01:14 +0100

    Added settings window and changed settings and made auto-save public

--- src/settings.rs
+++ src/settings.rs
@@ -11,12 +11,30 @@ pub enum Pf2eVersion {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[derive(Serialize, Deserialize, Hash)]
 pub struct Settings {
-    pub pf2e_version: Pf2eVersion,
-    pub undo_size: usize,
+    pf2e_version: Pf2eVersion,
+    undo_size: usize,
 }
 
 impl Default for Settings {
     fn default() -> Self {
         Self { pf2e_version: Pf2eVersion::default(), undo_size: 128 }
+    }
+}
+
+impl Settings {
+    pub fn get_undo_size(&self) -> usize {
+        self.undo_size
+    }
+
+    pub fn set_undo_size(&mut self, value: usize) {
+        self.undo_size = value
+    }
+
+    pub fn get_pf2e_version(&self) -> Pf2eVersion {
+        self.pf2e_version
+    }
+
+    pub fn set_pf2e_version(&mut self, value: Pf2eVersion) {
+        self.pf2e_version = value
     }
 }
